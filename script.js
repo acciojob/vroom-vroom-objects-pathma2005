2c3f4d91f3b374e4de8f853ec4ddb6bd0cd58cf2
Pathmasri E <pathmasri1372005@gmail.com>
2025-10-15 18:23:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 // Complete the js code
 function Car(make, model) {
-	this.make=make;
-	this.model=model;
+  this.make = make;
+  this.model = model;
 }
-car.prototype.getMakeModel=function() {
-	return this.make+" "+this.model;
-}
+Car.prototype.getMakeModel = function() {
+  return this.make + " " + this.model;
+};
 function SportsCar(make, model, topSpeed) {
-	car.call(this,make,model);
-	this.topSpeed=topSpeed;
+	Car.call(this, make, model);
+  this.topSpeed = topSpeed;
 }
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
-Sportscar.prototype.getTopSpeed=function() {
-	return this.topSpped;
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
+
 	
 }
 // Do not change the code below
