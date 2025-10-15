5e1a8a8038eab100a8d229945a2d6b7af104a872
Pathmasri E <pathmasri1372005@gmail.com>
2025-10-15 18:21:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // Complete the js code
-function Car(make, model) {}
-
-function SportsCar(make, model, topSpeed) {}
-
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+car.prototype.getMakeModel=function() {
+	return this.make+" "+this.model;
+}
+function SportsCar(make, model, topSpeed) {
+	car.call(this,make,model);
+	this.topSpeed=topSpeed;
+}
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+Sportscar.prototype.getTopSpeed=function() {
+	return this.topSpped;
+	
+}
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
