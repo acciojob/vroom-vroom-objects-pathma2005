441f36a78a02eb6c6d6745ce3d18b60266279379
Pathmasri E <pathmasri1372005@gmail.com>
2025-10-16 19:13:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 // Complete the js code
-function makeChange(amount) {
-  let change = {};
-
-  // Calculate quarters
-  change.q = Math.floor(amount / 25);
-  amount = amount % 25;
-
-  // Calculate dimes
-  change.d = Math.floor(amount / 10);
-  amount = amount % 10;
-
-  // Calculate nickels
-  change.n = Math.floor(amount / 5);
-  amount = amount % 5;
-
-  // Remaining are pennies
-  change.p = amount;
-
-  return change;
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
 }
+
+// Method on Car prototype
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`;
+};
+
+// SportsCar constructor (inherits from Car)
+function SportsCar(make, model, topSpeed) {
+  // Call the Car constructor to set make and model
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+
+// Inherit from Car prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Fix the constructor reference
+SportsCar.prototype.constructor = SportsCar;
+
+// Add method specific to SportsCar
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
